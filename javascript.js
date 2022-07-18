d54aaa8afbdbdaf545490a4e3f01e859a900b046
arkandria <miguelfariasgp@gmail.com>
2022-07-18 13:53:32 -0500

    achieve basic functionality, 5 tasks completed

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+let container = document.querySelector('.container');
 
     for (i=0; i<=255; i++) {
         
@@ -9,6 +9,29 @@ const container = document.querySelector('.container');
             pixel.style.backgroundColor = 'black';
        } );
     }
-    
-    
+ let  numberRows;
+const resetBtn = document.querySelector('.resetBtn');
+resetBtn.classList.add('resetBtn');
+resetBtn.addEventListener('click', () => {
+    numberRows = prompt("Please type number of squares per side (up to 100)")
+    parseInt(numberRows);
+    if (numberRows<=100) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        };
+        console.log('erased');
+        
+            for (i=0; i<= (numberRows*numberRows)-1; i++ ) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            container.appendChild(pixel);
+            let size = (960/numberRows)-2 + "px";
+            pixel.style.width = size;
+            pixel.style.height = size;
+            pixel.addEventListener("mouseover", () => {
+                pixel.style.backgroundColor = 'black';
+        } );
+        }
+    }
+});
 
